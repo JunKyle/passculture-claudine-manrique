8a4ab1f5d395f9511cfaef19a07fb9b5d2e4bb5d
JunKyle <claudine.manrique@gmail.com>
2023-01-30 15:57:27 +0100

    Test technique Claudine manrique

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement.closest('a')).toHaveAttribute('href', 'https://create-react-app.dev/docs/running-tests/');
+});
+
+test('renders news section title', () => {
+  render(<App />);
+  const titleElement = screen.getByText('News');
+  expect(titleElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,38 @@ import "./App.css";
 
 import React from "react";
 import logo from "./logo-pass-culture.svg";
+import newsData from './news.json';
 
 function App() {
+  const news = newsData.news || [];
+  let selectCategories:string[] = [] 
+  if (news) {
+    for (let i = 0; i < news.length; i++) {
+      if (!selectCategories.includes(news[i].category)) {
+        selectCategories.push(news[i].category);
+      }
+    }
+  }
+  console.log('selectCategories', selectCategories);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <main>
+        <section className="News" id="news">
+          {news.length ? <h1>News</h1> : ''}
+          {news.map((item, index) => (
+            <div className="NewsItem" key={index}>
+              <span className="NewsItem__title">{item.title}</span>
+              <span className="NewsItem__category">{item.category}</span>
+              <span className="NewsItem__date">{item.date}</span>
+              <p className="NewsItem__description">{item.text}</p>
+            </div>
+          ))}
+        </section>
+        <a href="https://create-react-app.dev/docs/running-tests/">Learn react</a>
+      </main>
     </div>
   );
 }
